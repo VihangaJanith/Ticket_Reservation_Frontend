37299657763ce73302e3238b09cea2c984e7537a
amanda-nethmini <it20106874@my.sliit.lk>
2023-10-10 00:34:58 +0530

    trainfrontend

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/User/Login";
 import Signup from "./Pages/User/Signup";
+import TrainsList from "./Pages/Train Management/TrainsList";
+import AddTrain from "./Pages/Train Management/AddTrain";
+import AllTrains from "./Pages/Train Management/AllTrains";
+import EditTrain from "./Pages/Train Management/EditTrain";
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/trainsList" element={<TrainsList />} />
+          <Route path="/addtrain" element={<AddTrain />} />
+          <Route path="/alltrains" element={<AllTrains />} />
+          <Route path="/edittrain/:id" element={<EditTrain />} />
         </Routes>
       </Router>
     </div>
